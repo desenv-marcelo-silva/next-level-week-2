17cb079d40943c1b3a6e8193a698e5eb738af7e7
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-08-03 23:31:06 -0300

    style(TeachersList): add style

--- src/pages/TeachersList/index.tsx
+++ src/pages/TeachersList/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import backIcon from '../../assets/images/icons/back.svg';
 import logoImg from '../../assets/images/logo.svg';
 
+import './styles.css';
+
 function TeachersList() {
   return (
     <div id="page-teacher-list" className="container">
