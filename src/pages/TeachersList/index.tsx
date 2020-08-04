ce5746746702e5c17f24c5f21c17c0307aea543b
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-08-04 00:42:41 -0300

    refactor(TeachersList): remove teacher-item

--- src/pages/TeachersList/index.tsx
+++ src/pages/TeachersList/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-
-import whatsApp from '../../assets/images/icons/whatsapp.svg';
+import TeacherItem from '../../components/TeacherItem';
 
 import './styles.css';
 
@@ -28,38 +27,9 @@ function TeachersList() {
       </PageHeader>
 
       <main>
-        <article className="teacher-item">
-          <header>
-            <img
-              src="https://avatars3.githubusercontent.com/u/18222796?s=460&u=69b77bfeaf625a570eb0d5be6086cd1e3811262e&v=4"
-              alt="Marcelo Silva"
-            />
-            <div>
-              <strong>Marcelo Silva</strong>
-              <span>Computer science</span>
-            </div>
-          </header>
-          <p>
-            Entusiasta Python e principalmente Javascript.
-            <br />
-            <br />
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe non
-            ea voluptatum et, totam sapiente doloribus iusto dolorum, aspernatur
-            similique temporibus voluptatem quam maxime eius quaerat voluptate
-            corporis modi odio?
-          </p>
-
-          <footer>
-            <p>
-              Preço/hora
-              <strong>R$ 69,00</strong>
-            </p>
-            <button type="button">
-              <img src={whatsApp} alt="WhatsApp icon" />
-              Entrar em contato
-            </button>
-          </footer>
-        </article>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
       </main>
     </div>
   );
