3b267f17f8f455583cf79de3244dc5ad2846b6ad
Marcelo Silva (mac) <desenv.marcelo.silva@gmail.com>
2020-08-05 23:36:38 -0300

    chore(landing): update number of connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -9,8 +9,18 @@ import giveClassesIcon from '../../assets/images/icons/study.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = React.useState(0);
+
+  React.useEffect(() => {
+    api.get('connections').then((response) => {
+      const { total } = response.data;
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -35,7 +45,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas&nbsp;
+          Total de {totalConnections} conexões já realizadas&nbsp;
           <img src={purpleHeartIcon} alt="Coração roxo por conexões" />
         </span>
       </div>
